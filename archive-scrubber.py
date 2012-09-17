0cb0926d0ca90b9e7cff0d56d3b2f067ede05030
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2012-09-17 22:22:45 +0100

    A few notes, nothing exciting

---
+++ archive-scrubber.py
@@ -0,0 +1,13 @@
+
+# archive starts page 1
+
+archive = "http://ratholeradio.org/feed/podcast/?format=ogg&paged=%i"
+page_number = 1
+page = ""
+
+
+while 1:
+    # get feed at page
+    # store page
+    # compare to last page
+        # if the same break, we have all the pages
