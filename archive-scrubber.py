c1b2c40efba20a97826367700bdee130a3f7d8ea
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2012-09-19 00:33:33 +0100

    Example xml and html, will save bashing RRorg while testing

--- archive-scrubber.py
+++ archive-scrubber.py
@@ -1,4 +1,4 @@
-
+import xml.etree.ElementTree as etree
 # archive starts page 1
 
 archive = "http://ratholeradio.org/feed/podcast/?format=ogg&paged=%i"
@@ -9,5 +9,8 @@ page = ""
 while 1:
     # get feed at page
     # store page
+    feed_tree = etree.fromstring()
     # compare to last page
         # if the same break, we have all the pages
+    for item in feed_tree.find("channel").findall("item"):
+        item.find("{http://purl.org/rss/1.0/modules/content/}encoded").text
