0bf66292b4a89d5a1f5c95394876b0c960bd462a
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2012-09-20 23:44:47 +0100

    Start using BeautifulSoup
    Turns out that each timestamp for the songs don't always match up with a <p>.
    This will make things a little difficult.

--- archive-scrubber.py
+++ archive-scrubber.py
@@ -1,16 +1,31 @@
+import time
+from BeautifulSoup import BeautifulSoup
 import xml.etree.ElementTree as etree
 # archive starts page 1
 
 archive = "http://ratholeradio.org/feed/podcast/?format=ogg&paged=%i"
 page_number = 1
-page = ""
+page = ''
 
 
 while 1:
     # get feed at page
     # store page
-    feed_tree = etree.fromstring()
+
+    #pretend feed:
+    feed = open('rss-example.xml').read()
+    
+    feed_tree = etree.fromstring(feed)
     # compare to last page
         # if the same break, we have all the pages
-    for item in feed_tree.find("channel").findall("item"):
-        item.find("{http://purl.org/rss/1.0/modules/content/}encoded").text
+
+    # Parse xml from feed
+    for item in feed_tree.find('channel').findall('item'):
+        html = BeautifulSoup(item.find('{http://purl.org/rss/1.0/modules/content/}encoded').text)
+        for p in html.findAll('p'):
+            # pick out timestamps for songs and parse strings
+            # each time stamp doesn't occur with a <p> :(
+            print p.getText()
+            print "====="
+        break
+    break
