d5ca8740870116b11a595e519ce29f32199229a1
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2012-11-20 02:58:50 +0000

    Archive scrubber mostly complete, now in a usable state

--- archive-scrubber.py
+++ archive-scrubber.py
@@ -3,18 +3,26 @@ import time, re, urllib2, json
 from BeautifulSoup import BeautifulSoup
 import xml.etree.ElementTree as etree
 
-archive = ["http://ratholeradio.org/category/podcast/feed/?paged=%i"] #,"http://ratholeradio.org/category/uncategorized/feed/?paged=%i","http://ratholeradio.org/category/live/feed/?paged=%i","http://ratholeradio.org/category/interview-2/feed/?paged=%i"]
+archive = ["http://ratholeradio.org/category/podcast/feed/?paged=%i","http://ratholeradio.org/category/uncategorized/feed/?paged=%i","http://ratholeradio.org/category/live/feed/?paged=%i","http://ratholeradio.org/category/interview-2/feed/?paged=%i"]
 
 # set regexs outside the loops
-track_matching = re.compile("[0-9]{2}\:[0-9]{2}", re.MULTILINE)
-detail_matching = re.compile("&#8211;|&#8212;", re.MULTILINE)
-episodes = {}
+track_matching = re.compile("[0-9]{2}\:[0-9]{2}", re.MULTILINE) # find things like 12:45
+detail_matching = re.compile("&#8211;|&#8212;|-", re.MULTILINE) # split out dashes
+
+database_file = 'database.json'
+
+try:
+    database = open(database_file, 'r')
+    episodes = json.load(database, 'utf-8')
+except Exception, e:
+    print e
+    episodes = {}
 
 def get_tracks(found_tags):
+    """Pick out timestamps for songs and parse strings as best we can"""
     tracks = {}
     for tag in found_tags:
-        # pick out timestamps for songs and parse strings
-        # each time stamp doesn't occur with a <p> :(
+        # each time stamp doesn't occur with a <p> or a <li> :(
         times = track_matching.findall(tag.getText())
         details = track_matching.split(tag.getText())[-len(times):]
         i = 0
@@ -33,28 +41,36 @@ for url in archive:
     page_number = 1
 
     while 1:
-        """try:
-            # grab a page
+        try:
+            # Grab a page
             feed = urllib2.urlopen(url % page_number).read() 
             feed_tree = etree.fromstring(feed)
             page_number = page_number + 1
         except urllib2.HTTPError:
             print "No more pages on %s" % url
             break
-        """
+        
+        """# local 'feed' for debugging
         feed = open('rss-example.xml').read()
         feed_tree = etree.fromstring(feed)
+        """
         # Parse xml from feed
         for item in feed_tree.find('channel').findall('item'):
-            html = BeautifulSoup(item.find('{http://purl.org/rss/1.0/modules/content/}encoded').text)
             link = item.find('link').text
-            # grab tracks
+
+            # Test to see if we already have this show or not
+            try:
+                episodes[link]
+                continue
+            except KeyError:
+                print "We've not seen %s yet" % link
+
+            # Grab tracks
+            html = BeautifulSoup(item.find('{http://purl.org/rss/1.0/modules/content/}encoded').text)
             tracks = get_tracks(html.findAll('p'))
             tracks.update(get_tracks(html.findAll('li')))
             episodes[link] = tracks
-        break
 
-
-database = open('database.json', 'w')
-database.write(json.dumps(episodes, indent=4))
+database = open(database_file, 'w')
+database.write(json.dumps(episodes, sort_keys=True, indent=4))
 database.close()
